865534f08dbaaff45531c6f1097de5c632f9edd6
Charles <charles@arti-zen.fr>
2024-10-31 17:16:13 +0100

    add swagger prop + remove generic GET

--- src/suggest/suggest.interfaces.ts
+++ src/suggest/suggest.interfaces.ts
@@ -1,7 +1,9 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SuggestPostDto {
+  @ApiProperty()
   @Type(() => String)
   @IsString()
   @IsNotEmpty()
